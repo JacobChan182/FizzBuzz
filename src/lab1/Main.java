4f174c583178452d945a48884ddf76a5fd631645
Jacob Chan <jacobchan@Chans-MacBook-Pro.local>
2025-09-08 11:09:24 -0400

    made Multiples class public

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,6 +3,6 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
-        Multiples.multiples(3, 5, 1000);
+        System.out.println(Multiples.multiples());
     }
 }
